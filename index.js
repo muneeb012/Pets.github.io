674bdf13f797688e268b3d515f6966200b5be820
muneeb012 <107207381+muneeb012@users.noreply.github.com>
2023-08-26 12:17:31 +0500

    Pets website

---
+++ index.js
@@ -0,0 +1,9 @@
+function play(){
+ document.getElementById("vid").style.display="none"
+ document.getElementById("player").style.display="inline"
+}
+function video2(){
+    document.getElementById("vid2").style.display="none"
+    document.getElementById("player2").style.display="inline"
+   }
+   
